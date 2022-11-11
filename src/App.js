a0fcecee4ddf641eaa023571db694aa15168ca79
Cupskrrt <khanalfarizzi@hotmail.com>
2022-11-11 23:50:46 +0700

    Creating App & Adding HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Text from "./components/Text";
+import Sponsors from "./components/Sponsors";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Hero />
+      <Text />
+      <Sponsors />
     </div>
   );
 }
